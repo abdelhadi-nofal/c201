0e9d8c28fb6c250f5e2fec0038aee4011c4396d6
abdelhadi-nofal <hadinofal1993@gmail.com>
2021-03-01 18:06:06 +0200

    js_add

--- js/app.js
+++ js/app.js
@@ -1,61 +1,60 @@
 let userInput1=prompt('What`s Your Name ?');
+alert('You are Welcome  '+userInput1);
+document.write('<h1>welcome  ' +userInput1 +'</h1>');
+alert('Answer be(yes/y-no/n)');
 
 let yea='yes';
 let yep='y';
 let noa='no';
 let nop='n';
 
-let userInput2=prompt('Do You Want To Know About This Webpage DEveloper ?').toLowerCase();
+let userInput2=prompt('IS 5>3').toLowerCase();
 while (true) {
   if(userInput2===yea){
-    alert('You are Welcome  '+userInput1);
+    alert('You are CORRECT  ');
     console.log('welcome');
-    document.write('<h1>welcome  ' +userInput1 +'</h1>');
     break;
   }
   else if (userInput2===yep){
-    alert('You are Welcome  '+userInput1);
     console.log('welcome');
-    document.write('<h1>welcome  ' +userInput1 +'</h1>');
+    alert('You are CORRECT  ');
     break;
 
   }
   else if (userInput2===noa){
-    alert('as u like');
+    alert('WRONG');
     break;
 
   }
   else if (userInput2===nop){
-    alert('as u like');
+    alert('WRONG');
     break;
-
   }
   else {
     break;
   }
 }
 
-let Age=prompt('Do You Want To Know My Age ?').toLowerCase();
+let Age=prompt('IS 100<10.1').toLowerCase();
 while (true) {
   if(Age===yea){
-    alert('You are Welcome To Know it  '+userInput1);
+    alert('WRONG  ');
     console.log('welcome');
-    document.write('<h1>welcome  ' +userInput1 +'</h1>');
     break;
   }
   else if (Age===yep){
-    alert('You are Welcome To Know it ');
+    alert('WRONG ');
     console.log('age');
     break;
 
   }
-  else if (userInput2===noa){
-    alert('as u like');
+  else if (Age===noa){
+    alert('You are CORRECT');
     break;
 
   }
-  else if (userInput2===nop){
-    alert('as u like');
+  else if (Age===nop){
+    alert('You are CORRECT');
     break;
 
   }
@@ -64,26 +63,26 @@ while (true) {
   }
 }
 
-let major=prompt('Do You Want To Know My Major ?').toLowerCase();
+let major=prompt('Is 4!=24').toLowerCase();
 while (true) {
   if(major===yea){
-    alert('OK I WILL TALK ABOUT MY MAJOR  '+userInput1);
+    alert('You are CORRECT  '+userInput1);
     console.log('welcome');
     break;
   }
   else if (major===yep){
-    alert('OK I WILL TALK ABOUT MY MAJOR ');
+    alert('You are CORRECT ');
     console.log('major');
     break;
 
   }
-  else if (userInput2===noa){
-    alert('as u like');
+  else if (major===noa){
+    alert('WRONG');
     break;
 
   }
-  else if (userInput2===nop){
-    alert('as u like');
+  else if (major===nop){
+    alert('WRONG');
     break;
 
   }
@@ -92,26 +91,26 @@ while (true) {
   }
 }
 
-let uni=prompt('Do You Want To Know Where did I Study ?').toLowerCase();
+let uni=prompt('Is 0!=1').toLowerCase();
 while (true) {
   if(uni===yea){
-    alert('OK I WILL Tell You  '+userInput1);
+    alert('You are CORRECT  '+userInput1);
     console.log('welcome');
     break;
   }
   else if (uni===yep){
-    alert('OK I WILL Tell You ');
+    alert('You are CORRECT ');
     console.log('uni');
     break;
 
   }
-  else if (userInput2===noa){
-    alert('as u like');
+  else if (uni===noa){
+    alert('WRONG');
     break;
 
   }
-  else if (userInput2===nop){
-    alert('as u like');
+  else if (uni===nop){
+    alert('WRONG');
     break;
 
   }
@@ -120,27 +119,27 @@ while (true) {
   }
 }
 
-let ltuc=prompt('Do You Want To Know What Iam doing  ?').toLowerCase();
+let ltuc=prompt('Is 1!=0').toLowerCase();
 while (true) {
   if(ltuc===yea){
-    alert('OK I WILL Tell You  '+userInput1);
+    alert('WRONG  '+userInput1);
     console.log('welcome');
     break;
   }
   else if (ltuc===yep){
-    alert('OK I WILL Tell You ');
+    alert('WRONG ');
     console.log('ltuc');
     break;
 
   }
 
   else if (userInput2===noa){
-    alert('as u like');
+    alert('You are CORRECT');
     break;
 
   }
   else if (userInput2===nop){
-    alert('as u like');
+    alert('You are CORRECT');
     break;
 
   }
