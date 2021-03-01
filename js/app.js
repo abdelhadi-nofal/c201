dbf0c31a8d76d145b2401ea0d8e9ba47ed8b4cb7
abdelhadi-nofal <hadinofal1993@gmail.com>
2021-03-01 15:53:44 +0200

    craeating_aboutme_page

---
+++ js/app.js
@@ -0,0 +1,155 @@
+let userInput1=prompt('What`s Your Name ?');
+
+let yea='yes';
+let yep='y';
+let noa='no';
+let nop='n';
+
+let userInput2=prompt('Do You Want To Know About This Webpage DEveloper ?').toLowerCase();
+while (true) {
+  if(userInput2===yea){
+    alert('You are Welcome  '+userInput1);
+    console.log('welcome');
+    document.write('<h1>welcome  ' +userInput1 +'</h1>');
+    break;
+  }
+  else if (userInput2===yep){
+    alert('You are Welcome  '+userInput1);
+    console.log('welcome');
+    document.write('<h1>welcome  ' +userInput1 +'</h1>');
+    break;
+
+  }
+  else if (userInput2===noa){
+    alert('as u like');
+    break;
+
+  }
+  else if (userInput2===nop){
+    alert('as u like');
+    break;
+
+  }
+  else {
+    break;
+  }
+}
+
+let Age=prompt('Do You Want To Know My Age ?').toLowerCase();
+while (true) {
+  if(Age===yea){
+    alert('You are Welcome To Know it  '+userInput1);
+    console.log('welcome');
+    document.write('<h1>welcome  ' +userInput1 +'</h1>');
+    break;
+  }
+  else if (Age===yep){
+    alert('You are Welcome To Know it ');
+    console.log('age');
+    break;
+
+  }
+  else if (userInput2===noa){
+    alert('as u like');
+    break;
+
+  }
+  else if (userInput2===nop){
+    alert('as u like');
+    break;
+
+  }
+  else {
+    break;
+  }
+}
+
+let major=prompt('Do You Want To Know My Major ?').toLowerCase();
+while (true) {
+  if(major===yea){
+    alert('OK I WILL TALK ABOUT MY MAJOR  '+userInput1);
+    console.log('welcome');
+    break;
+  }
+  else if (major===yep){
+    alert('OK I WILL TALK ABOUT MY MAJOR ');
+    console.log('major');
+    break;
+
+  }
+  else if (userInput2===noa){
+    alert('as u like');
+    break;
+
+  }
+  else if (userInput2===nop){
+    alert('as u like');
+    break;
+
+  }
+  else {
+    break;
+  }
+}
+
+let uni=prompt('Do You Want To Know Where did I Study ?').toLowerCase();
+while (true) {
+  if(uni===yea){
+    alert('OK I WILL Tell You  '+userInput1);
+    console.log('welcome');
+    break;
+  }
+  else if (uni===yep){
+    alert('OK I WILL Tell You ');
+    console.log('uni');
+    break;
+
+  }
+  else if (userInput2===noa){
+    alert('as u like');
+    break;
+
+  }
+  else if (userInput2===nop){
+    alert('as u like');
+    break;
+
+  }
+  else {
+    break;
+  }
+}
+
+let ltuc=prompt('Do You Want To Know What Iam doing  ?').toLowerCase();
+while (true) {
+  if(ltuc===yea){
+    alert('OK I WILL Tell You  '+userInput1);
+    console.log('welcome');
+    break;
+  }
+  else if (ltuc===yep){
+    alert('OK I WILL Tell You ');
+    console.log('ltuc');
+    break;
+
+  }
+
+  else if (userInput2===noa){
+    alert('as u like');
+    break;
+
+  }
+  else if (userInput2===nop){
+    alert('as u like');
+    break;
+
+  }
+  else {
+    break;
+  }
+}
+
+
+
+
+
